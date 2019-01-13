821ec3a3ad9dd70fbc76e3953123801deaae462f
AlexTsymball <alya.tsymbal@gmail.com>
2019-01-13 14:48:33 +0200

    Initial commit

---
+++ src/asd/sfa.java
@@ -0,0 +1,7 @@
+package asd;
+
+public class sfa {
+    public static void main(String[] args) {
+        System.out.printf("no");
+    }
+}
---
+++ src/zxc/qaz.java
@@ -0,0 +1,7 @@
+package zxc;
+
+public class qaz {
+    public static void main(String[] args) {
+        System.out.printf("yes");
+    }
+}
